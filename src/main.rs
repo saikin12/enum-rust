5d7379b4d7c77b8c58a1e2dc781c1e8e4cd202ef
saikin12 <eken.apache@gmail.com>
2026-03-09 22:35:21 -0700

    update statusHttp

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ fn main() {
         Arah::selatan=>println!("bergerak ke selatan"),
         Arah::timur=>println!("bergerak ke timur"),
         Arah::barat=>println!("bergerak ke barat"),
-
+       
     }
+     println!("{}", deskripsi_status(StatusHTTP::NotFound ));
+
 }
 
 
@@ -22,4 +24,14 @@ enum StatusHTTP {
      NotFound,
      InternalServerError,
      Unauthorized,
+}
+fn deskripsi_status(status:StatusHTTP)->&'static str{
+    match status{
+        StatusHTTP::Ok =>"200 - berhasil",
+        StatusHTTP::NotFound =>"404 - tidak ditemukan",
+        StatusHTTP::InternalServerError => "500 error server",
+        StatusHTTP::Unauthorized => "401 -tidak ditemukan",
+
+
+    }
 }
